293391356c92164aaf7f40a2371892f50ddbdb1b
thomasdunlap <tom.a.dunlap@gmail.com>
2018-01-26 22:45:38 -0500

    feat: Added Network instance.

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,7 +10,11 @@ adventureland = media.Movie("Adventureland", "In the summer of 1987, a college g
                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI1NTYyMjA2N15BMl5BanBnXkFtZTcwNjU1OTA0Mg@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
                             "https://www.youtube.com/watch?v=gfPE_MEKipM")
 
+network = media.Movie("Network", "A television network cynically exploits a deranged former anchor's ravings and revelations about the news media for its own profit.",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BYTc3NjcyYmYtMTI4OC00OGViLTkwYzQtZWU2MGMwMTI5ZTU4XkEyXkFqcGdeQXVyNTc1NTQxODI@._V1_.jpg",
+                            "https://www.youtube.com/watch?v=qnGgsJ26dao")
+
 # Movie instances to convert to HTML elements
-movies = [donnie_darko, adventureland]
+movies = [network, donnie_darko, adventureland]
 
 fresh_tomatoes.open_movies_page(movies)
